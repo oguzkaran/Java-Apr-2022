9f9007eb3a685b45438dc722109af2459dc6a99b
Oğuz Karan <oguzkaran@csystem.org>
2022-05-15 20:07:08 +0300

    Update

--- src/eclipseworkspce/Sample/src/csd/App.java
+++ src/eclipseworkspce/Sample/src/csd/App.java
@@ -1,26 +1,16 @@
 /*----------------------------------------------------------------------------------------------------------------------
-	Aşağıdaki örneği çalıştırıp yuvarlama hatasını gözlemleyiniz. Örneğin detaylarını şu an önemsemeyiniz. İleride
-	hepsi ele alınacaktır
+	Anahatar Notlar: Her ne kadar değişken isimlendirmede UNICODE alfabetik karakterler kullanılabilse de programlamada
+	yalnızca İngilizce alfabedeki 26 karakterin kulanılması iyi bir tekniktir. Ayrıca değişken isimlerine ilişkin kelimelerin de
+	İngilizce olarak seçilmesi önerilir. 
 -----------------------------------------------------------------------------------------------------------------------*/
 package csd;
 
 class App {
 	public static void main(String [] args)
-	{
-		float a = 0.1F, b = 0.2F;
-		float c;
+	{		
+		int öğrencisayısı = 10;			
 		
-		
-		c = a + b;
-		
-		System.out.printf("a = %.20f%n", a);
-		System.out.printf("b = %.20f%n", b);
-		System.out.printf("c = %.20f%n", c);
-		
-		if (c == 0.3)
-			System.out.println("Eşit");
-		else
-			System.out.println("Eşit değil");
+		System.out.println(öğrencisayısı);
 	}
 }
 
