e6f26a76ab993a98c14e131ed7056b5788b3ef89
Oğuz Karan <oguzkaran@csystem.org>
2022-05-15 20:07:29 +0300

    Update

--- src/eclipseworkspce/Sample/src/csd/App.java
+++ src/eclipseworkspce/Sample/src/csd/App.java
@@ -1,7 +1,7 @@
 /*----------------------------------------------------------------------------------------------------------------------
 	Anahatar Notlar: Her ne kadar değişken isimlendirmede UNICODE alfabetik karakterler kullanılabilse de programlamada
 	yalnızca İngilizce alfabedeki 26 karakterin kulanılması iyi bir tekniktir. Ayrıca değişken isimlerine ilişkin kelimelerin de
-	İngilizce olarak seçilmesi önerilir. 
+	İngilizce olarak seçilmesi önerilir. Biz de bu "convention"'a uygun kodlar yazacağız
 -----------------------------------------------------------------------------------------------------------------------*/
 package csd;
 
