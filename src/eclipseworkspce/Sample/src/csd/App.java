d582b271f5f9fbb76923945b1722410c94dd77ba
Oğuz Karan <oguzkaran@csystem.org>
2022-05-29 18:32:47 +0300

    Update

--- src/eclipseworkspce/Sample/src/csd/App.java
+++ src/eclipseworkspce/Sample/src/csd/App.java
@@ -1,51 +1,20 @@
-/*----------------------------------------------------------------------------------------------------------------------
-	Sınıf Çalışması: Klavyeden girilen iki tane int türden sayının toplamını, çarpımını ve farkını aşağıdaki gibi
-	ekrana basan programı yazınız:
-	Girilen sayılar 10 ve 20 ise
-		10 + 20 = 30
-		10 * 20 = 200
-		10 - 20 = -10
-	şeklinde ekran çıktısı olacaktır
+/*-----------------------------------------------------------------------------------------------------------------------
+	  Math sınıfının min ve max metotları
 -----------------------------------------------------------------------------------------------------------------------*/
 package csd;
 
 class App {
 	public static void main(String [] args)
-	{	
-		FindSumMultiplySubtractApp.run();		
-	}
-}
-
-class FindSumMultiplySubtractApp {
-	public static void run()
 	{
 		java.util.Scanner kb = new java.util.Scanner(System.in);
 		System.out.print("Birinci sayıyı giriniz:");
-		int a = Integer.parseInt(kb.nextLine());
+		double a = Double.parseDouble(kb.nextLine());
 		
 		System.out.print("İkinci sayıyı giriniz:");
-		int b = Integer.parseInt(kb.nextLine());
+		double b = Double.parseDouble(kb.nextLine());		
 		
-		System.out.printf("%d + %d = %d%n", a, b, NumberUtil.sum(a, b));
-		System.out.printf("%d * %d = %d%n", a, b, NumberUtil.multiply(a, b));
-		System.out.printf("%d - %d = %d%n", a, b, NumberUtil.subtract(a, b));
+		System.out.printf("min(%f, %f) = %f%n", a, b, Math.min(a, b));
+		System.out.printf("max(%f, %f) = %f%n", a, b, Math.max(a, b));
 	}
 }
 
-
-class NumberUtil {
-	public static int sum(int a, int b)
-	{
-		return a + b;
-	}
-	
-	public static int multiply(int a, int b)
-	{
-		return a * b;
-	}
-	
-	public static int subtract(int a, int b)
-	{
-		return a - b;
-	}
-}
