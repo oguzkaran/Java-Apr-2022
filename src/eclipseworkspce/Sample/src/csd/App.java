a2bb1e1fc607c7111fe355185fe7b4cc4a708161
Oğuz Karan <oguzkaran@csystem.org>
2022-06-11 18:34:00 +0300

    Update

--- src/eclipseworkspce/Sample/src/csd/App.java
+++ src/eclipseworkspce/Sample/src/csd/App.java
@@ -1,18 +1,18 @@
 /*-----------------------------------------------------------------------------------------------------------------------	
-	Java'da void bir metot çağrısı "void expression" olarak ele alınır. Yani bir ifadedir ancak bir değer üretmez
+	! operatörü flag değişkenlerle aşağıdaki gibi kullanılabilir. Kod sadece durumu göstermek için yazılmıştır	
 -----------------------------------------------------------------------------------------------------------------------*/
 package csd;
 
 class App {
 	public static void main(String [] args)
 	{
-		NumberUtil.print(10, 20);
-	}
-}
-
-class NumberUtil {
-	public static void print(int a, int b)
-	{
-		System.out.printf("%d, %d%n", a, b);
+		boolean flag = false;
+		
+		//...
+		
+		flag = !flag;
+		
+		
+		System.out.printf("flag = %b%n", flag);
 	}
 }
