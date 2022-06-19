19632d35fbc7e1e939188cbc8b0f863c9dd22c81
Oğuz Karan <oguzkaran@csystem.org>
2022-06-19 22:21:35 +0300

    Update

--- src/eclipseworkspce/Sample/src/csd/App.java
+++ src/eclipseworkspce/Sample/src/csd/App.java
@@ -1,19 +1,17 @@
 /*-----------------------------------------------------------------------------------------------------------------------	
-	for döngü deyimi birinci kısmında bildirilen değişken for döngü deyimi boyunca görülebilirdir
+	Aşağıdaki örnekte 3 basamaklı Armstrong sayıları içiçe döngü ile ekrana bastırılmıştır. İçiçe döngüyü göstermek
+	için kod bu şekilde yazılmıştır
 -----------------------------------------------------------------------------------------------------------------------*/
 package csd;
 
 class App {
 	public static void main(String [] args)
 	{
-		java.util.Scanner kb = new java.util.Scanner(System.in);
-		System.out.print("Bir sayı giriniz:");
-		int n = kb.nextInt();	
-		
-		for (int i = 0; i < n; ++i)
-			System.out.printf("%d ", i);
-		
-		System.out.println();
-		
+		for (int a = 1; a <= 9; ++a)
+			for (int b = 0; b <= 9; ++b)
+				for (int c = 0; c <= 9; ++c)
+					if (100 * a + 10 * b + c == a * a * a + b * b * b + c * c * c)
+						System.out.printf("%d%d%d%n", a, b, c);
 	}
 }
+
