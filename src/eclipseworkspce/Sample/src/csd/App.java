aa38d7beed187a634174926511fb774a1717678c
Oğuz Karan <oguzkaran@csystem.org>
2022-06-25 18:29:20 +0300

    Update

--- src/eclipseworkspce/Sample/src/csd/App.java
+++ src/eclipseworkspce/Sample/src/csd/App.java
@@ -1,17 +1,66 @@
 /*-----------------------------------------------------------------------------------------------------------------------	
-	Aşağıdaki örnekte 3 basamaklı Armstrong sayıları içiçe döngü ile ekrana bastırılmıştır. İçiçe döngüyü göstermek
-	için kod bu şekilde yazılmıştır
+	Sınıf Çalışması: Parametresi ile aldığı int türden bir sayıdan büyük ilk asal sayıyı döndüren getNextPrime isimli 
+	metodu yazınız ve aşağıdaki kod ile test ediniz
+	Açıklamalar:
+		- isPrime metodunun hızlı versiyonunun, "hızlı" olduğu bilinciyle getPrime metodunu yazınız				
 -----------------------------------------------------------------------------------------------------------------------*/
 package csd;
 
 class App {
 	public static void main(String [] args)
 	{
-		for (int a = 1; a <= 9; ++a)
-			for (int b = 0; b <= 9; ++b)
-				for (int c = 0; c <= 9; ++c)
-					if (100 * a + 10 * b + c == a * a * a + b * b * b + c * c * c)
-						System.out.printf("%d%d%d%n", a, b, c);
+		GetNextPrimeTest.run();
+	}
+}
+
+class GetNextPrimeTest {
+	public static void run()
+	{
+		java.util.Scanner kb = new java.util.Scanner(System.in);
+		
+		for (;;) {
+			System.out.print("Bir sayı giriniz:");
+			int n = Integer.parseInt(kb.nextLine());
+			
+			if (n <= 0) {
+				System.out.println("Tekrar yapıyor musunuz?");
+				return;
+			}
+			
+			System.out.printf("%d. asal sayı:%d%n", n, NumberUtil.getPrime(n));
+		}
+		
+	}
+}
+
+class NumberUtil {
+	public static long getNextPrime(int n)
+	{		
+		//TODO:
+	}
+	
+	public static boolean isPrime(long a)
+	{
+		if (a <= 1)
+			return false;
+		
+		if (a % 2 == 0)
+			return a == 2;
+		
+		if (a % 3 == 0)
+			return a == 3;
+		
+		if (a % 5 == 0)
+			return a == 5;
+		
+		if (a % 7 == 0)
+			return a == 7;
+		
+		for (long i = 11; i * i <= a; i += 2)
+			if (a % i == 0)
+				return false;
+		
+		return true;
 	}
 }
 
