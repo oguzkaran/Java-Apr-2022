66c41d0e3cb361bdc9533dd59a4bb79a8ce66c78
Oğuz Karan <oguzkaran@csystem.org>
2022-07-03 18:44:09 +0300

    Update

--- src/eclipseworkspce/Sample/src/csd/App.java
+++ src/eclipseworkspce/Sample/src/csd/App.java
@@ -1,38 +1,59 @@
 /*-----------------------------------------------------------------------------------------------------------------------	
-	Farklı Türlerin birbirine atanması (Tür dönüşümleri-type conversions)
-	T1 ve T2 birer tür olmak üzere T1 türünden, T2 türüne yapılan atama işlemine T1 türünden T2 türüne 
-	"doğrudan dönüşüm (implicit conversion)" denir. Atama işlemi de aynı zamanda bir dönüşümdür. Örneğin bu işlem
-	T1 a;
-	T2 b;
-	
-	a = b;
-	
-	şeklinde belirtilebilir. Konu içerisinde anlatılacak olan istisna durumlar dışında tüm atama işlemleri için bu 
-	tanım geçerlidir. Bu durumlar ayrıca ele alınacaktır
-	
-	Yukarıdaki atama işleminde T2 türüne "kaynak tür (source type)", T1 türüne de "hedef tür (target type)" denir.
-	Java'da kaynak türden hedef türe implicit dönüşümünün (atamanın) yapılıp yapılamacağı derleme zamanından belirlenir.
-	Kaynak türe ilişkin değere çalışma zamanında bakılarak karar verilmez. Zaten çoğu zaman da bu değer derleme zamanında
-	bilinemez. Yani Java'da hangi türlerin hangi türlere doğrudan dönüşebildiği belirlidir.
-	
-	Java'da genel olarak bilgi kaybına yol açmayacak doğrudan dönüşümler geçerlidir
-	
-	Anahtar Notlar: "Java Language Specification"'da temel türler (primitive types) arasında, uzunluk (size) olarak büyük olan türden,
-	uzunluk olarak küçük olan türe yapılan dönüşüme "daraltma dönüşümü (narrowing conversion)", uzunluk olarak küçük
-	olan türden büyük olan türe yapılan dönüşüme "genişletme dönüşümü (widening conversion)" denir. 
-	
-	Doğrudan dönüşüme ilişkin ayrıntılar konu içerisinde anlatılacaktır
+	Homework-002-5. sorunun bir çözümü
+	(Not: Çözüm çalışma sorusunun verildiği tarihte işlenmiş olan konulara göre yazılmıştır)
 -----------------------------------------------------------------------------------------------------------------------*/
 package csd;
 
 class App {
 	public static void main(String [] args)
 	{	
-		java.util.Scanner kb = new java.util.Scanner(System.in);
-		long a = kb.nextLong();		
-		int b;
-		
-		b = a; //error
+		BallFallApp.run();
 	}
 }
 
+class BallFallApp {
+	public static void run()
+	{
+		java.util.Scanner kb = new java.util.Scanner(System.in);
+		
+		for (;;) {
+			System.out.print("Input width and height:");
+			int width = kb.nextInt();
+			int height = kb.nextInt();
+			
+			if (width == 0)
+				break;
+			
+			BallFall.play(width, height);			
+		}
+		
+		System.out.println("Tekrar yapıyor musunuz?");
+	}	
+}
+
+class NumberUtil {
+	
+	public static boolean isPrime(long a)
+	{
+		if (a <= 1)
+			return false;
+		
+		if (a % 2 == 0)
+			return a == 2;
+		
+		if (a % 3 == 0)
+			return a == 3;
+		
+		if (a % 5 == 0)
+			return a == 5;
+		
+		if (a % 7 == 0)
+			return a == 7;
+		
+		for (long i = 11; i * i <= a; i += 2)
+			if (a % i == 0)
+				return false;
+		
+		return true;
+	}
+}
