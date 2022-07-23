5209fcf93030adf40830bd892f5f304f8832872f
Oğuz Karan <oguzkaran@csystem.org>
2022-07-23 18:31:36 +0300

    Update

--- src/eclipseworkspce/Sample/src/csd/App.java
+++ src/eclipseworkspce/Sample/src/csd/App.java
@@ -1,13 +1,18 @@
 /*-----------------------------------------------------------------------------------------------------------------------	
-	char türünden short türüne doğrudan dönüşüm geçersizdir
+	İşlem Öncesi Otomatik Tür Dönüşümü:
+	İki operandlı bir operatör için derleyici önce operandların türüne bakar. Operandlar farklı türdense ve işlem
+	geçerli ise, ortak bir türe dönüştürülerek işlem yapılır. Hatta bazı durumlarda iki tür aynı olsa bile ikisini de
+	farklı türe dönüştürür. Yani derleyici buna yönelik kod üretir. Bu dönüştürmeye "işlem öncesi otomatik tür dönüşümü"
+	denir. 
+	
+	Bu kuralın özeti "Genel olarak külçük türe ilişkin değer büyük türe dönüştürülür. Sonuş büyük türden çıkar biçimindedir.
 -----------------------------------------------------------------------------------------------------------------------*/
 package csd;
 
 class App {
 	public static void main(String [] args)
-	{		
-			
-					
+	{	
+				
 	}
 }
 
