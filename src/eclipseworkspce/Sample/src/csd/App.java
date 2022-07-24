07af677138f0eeb9a3ddb56e66b5abfee34353af
Oğuz Karan <oguzkaran@csystem.org>
2022-07-24 20:46:29 +0300

    Update

--- src/eclipseworkspce/Sample/src/csd/App.java
+++ src/eclipseworkspce/Sample/src/csd/App.java
@@ -1,30 +1,22 @@
-/*-----------------------------------------------------------------------------------------------------------------------
-	Sınıf Çalışması: Klavyeden sıfır girilene kadar alınan int türden sayıların ortalamasını buşlan progamı yazınız:
-	Açıklama: Toplam değeri ve toplam giilen değerlerin sayısı int olarak tutulacaktır
+/*-----------------------------------------------------------------------------------------------------------------------	
 	
-	Aşağıdaki örnekte tür dönüştürme operatörü kullanılmazsa bilgi kaybı oluşabilir
 -----------------------------------------------------------------------------------------------------------------------*/
 package csd;
 
 class App {
 	public static void main(String [] args)
-	{	
-		FindAverageApp.run();
+	{
+		java.util.Scanner kb = new java.util.Scanner(System.in);
+		
+		System.out.print("Birinci sayıyı giriniz:");
+		int a = Integer.parseInt(kb.nextLine());
+		
+		System.out.print("İkinci sayıyı giriniz:");
+		int b = Integer.parseInt(kb.nextLine());		
+		
+		int max = a > b ? a : b;		
+		
+		System.out.printf("max(%d, %d) = %d%n", a, b, max);		
 	}
 }
 
-class FindAverageApp {
-	public static void run()
-	{
-		java.util.Scanner kb = new java.util.Scanner(System.in);
-		System.out.println("Sayıları girmeye başlayınız:");
-		int sum = 0, count = 0;
-		
-		for (int a = 0; (a = kb.nextInt()) != 0; ++count, sum += a)
-			;
-		
-		double average = (double)sum / count;
-		
-		System.out.printf("Ortalama:%f%n", average);
-	}
-}
