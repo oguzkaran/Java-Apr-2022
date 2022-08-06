1881faac7f783e61fd6d64e9139d0378c9ba9a9b
Oğuz Karan <oguzkaran@csystem.org>
2022-08-06 20:14:37 +0300

    Update

--- src/eclipseworkspce/Sample/src/csd/App.java
+++ src/eclipseworkspce/Sample/src/csd/App.java
@@ -1,13 +1,27 @@
 /*-----------------------------------------------------------------------------------------------------------------------	
-	Değişkenlerin ömürleri (storage duration): Bir değişkenin ömrü bellekte yer kapladığı zaman aralığıdır. Yani bir 
-	değişkenin ömrü o değişkenin bellekte yaratılmasıyla (yer ayrılmasıyla) yok edilmesi arasında geçen süredir. 	 
+	Sınıf bildirimi aslında bir tür bildirimi olduğuna göre sınıf türünden değişkenler bildirilebilir
 -----------------------------------------------------------------------------------------------------------------------*/
 package csd;
 
 class App {
 	public static void main(String [] args)
-	{
-					
+	{	
+		Sensor s;
+		Person p;
+		Device d;
+		
+		//...
 	}
 }
 
+class Sensor {
+	//...
+}
+
+class Person {
+	//...
+}
+
+class Device {
+	//...
+}
