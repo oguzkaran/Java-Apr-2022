e4caeec452f0efbad42bd9e3c45404006c17bf96
Oğuz Karan <oguzkaran@csystem.org>
2022-08-06 18:31:38 +0300

    Update

--- src/eclipseworkspce/Sample/src/csd/App.java
+++ src/eclipseworkspce/Sample/src/csd/App.java
@@ -1,48 +1,13 @@
 /*-----------------------------------------------------------------------------------------------------------------------	
-	Homework-003-8. sorunun bir çözümü
-	(Not: Çözümde koşul operatörü kullanılmıştır)
+	Değişkenlerin ömürleri (storage duration): Bir değişkenin ömrü bellekte yer kapladığı zaman aralığıdır. Yani bir 
+	değişkenin ömrü o değişkenin bellekte yaratılmasıyla (yer ayrılmasıyla) yok edilmesi arasında geçen süredir. 	 
 -----------------------------------------------------------------------------------------------------------------------*/
 package csd;
 
 class App {
 	public static void main(String [] args)
 	{
-		PrintCollatzTest.run();
-	}
-}
-
-class PrintCollatzTest {
-	public static void run()
-	{
-		java.util.Scanner kb = new java.util.Scanner(System.in);
-		
-		for (;;) {
-			System.out.print("Bir sayı giriniz:");
-			int a = Integer.parseInt(kb.nextLine());
-			
-			if (a == 0)
-				break;
-			
-			NumberUtil.printCollatz(a);
-		}
-		
-		System.out.println("Tekrar yapıyor musunuz?");
-	}
-}
-
-
-class NumberUtil {
-	public static void printCollatz(int n)
-	{
-		if (n <= 0) {
-			System.out.println("Geçersiz sayı");
-			return;
-		}
-		
-		System.out.println(n);
-		
-		while (n != 1) 
-			System.out.println(n = (n % 2 == 0) ? (n / 2) : (3 * n + 1));
+					
 	}
 }
 
