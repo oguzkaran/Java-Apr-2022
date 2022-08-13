9b951bc64a8036b8e3ee7edd6017bc583fab2220
Oğuz Karan <oguzkaran@csystem.org>
2022-08-13 20:36:37 +0300

    Update

--- src/eclipseworkspce/Sample/src/csd/App.java
+++ src/eclipseworkspce/Sample/src/csd/App.java
@@ -1,25 +1,80 @@
 /*-----------------------------------------------------------------------------------------------------------------------	
-	Sınıfın static bir veri elemanına sınıf dışından referans ve nokta operatörü ile erişilebilir. Sınıf ismi ile
-	erişmekten farkı yoktur. Sınıfın static veri elemanlarına bu şekilde erişim okunabilirlik/algılanabilirlik 
-	açısından edilmemelidir
+	Sınıf Çalışması: Katsayıları klavyeden girilen ikinci dereceden denklemin köklerini bulup ekrana yazdıran 
+	programı yazınız.
+	Açıklamalar:
+	- İki kök varsa örneğin 
+		x1 = 3, x2 = 4 
+	biçiminde ekrana yazılacaktır
+	- Çakışık kök varsa 
+		x1 = x2 = -4 
+	biçiminde ekrana yazılacaktır
+	- Gerçek kök yoksa 
+		Gerçek kök yok!... 
+	biçiminde ekrana yazılacaktır	
+	
+	
+	Sınıfın aşağıdaki gibi kullanım şekli sınıfların yararı 100 birim düşünülürse %1'den daha azdır. Şüphesiz örnek 
+	farklı yaklaşımlarla da yazılabilir. Sınıfın bir kullanımını göstermek için yazılmıştır. İleride daha iyisi yazılacaktır 
 -----------------------------------------------------------------------------------------------------------------------*/
 package csd;
 
 class App {
 	public static void main(String [] args)
-	{		
-		Sample s = new Sample();
-		
-		s.x = 10; //Gönlümüzün error'u
-		s.y = true; //Gönlümüzün error'u
-		
-		System.out.printf("Sample.x = %d%n", Sample.x);
-		System.out.printf("Sample.y = %b%n", Sample.y);
+	{
+		SecondOrderEquationSolverApp.run();		
 	}
 }
 
-class Sample {
-	public static int x;
-	public static boolean y;
-	//...
+class SecondOrderEquationSolverApp {
+	public static void run()
+	{
+		java.util.Scanner kb = new java.util.Scanner(System.in);
+		
+		System.out.print("İkinci dereceden denklemin katsayılarını giriniz:(a, b, c)");
+		double a = kb.nextDouble();		
+		double b = kb.nextDouble();
+		double c = kb.nextDouble();
+		
+		SecondOrderEquationRootInfo ri = SecondOrderEquationSolver.findRoots(a, b, c);
+		
+		if (ri.exists)
+			System.out.printf("x1 = %f, x2 = %f%n", ri.x1, ri.x2);
+		else
+			System.out.println("Gerçek kök yok");
+	}
+}
+
+class SecondOrderEquationRootInfo {
+	public double x1, x2;
+	public boolean exists;
+}
+
+class SecondOrderEquationSolver {
+	public static void doWorkForSuccess(SecondOrderEquationRootInfo ri, double delta, double a, double b)
+	{
+		double sqrtDelta = Math.sqrt(delta);
+		
+		ri.exists = true;
+		ri.x1 = (-b + sqrtDelta) / (2 * a);
+		ri.x2 = (-b - sqrtDelta) / (2 * a);
+	}
+	
+	
+	public static double getDelta(double a, double b, double c)
+	{
+		return b * b - 4 * a * c;
+	}
+	
+	public static SecondOrderEquationRootInfo findRoots(double a, double b, double c)
+	{
+		double delta = getDelta(a, b, c);
+		
+		SecondOrderEquationRootInfo ri = new SecondOrderEquationRootInfo();
+		
+		if (delta >= 0)
+			doWorkForSuccess(ri, delta, a, b);
+		
+		return ri;		
+	}
+	
 }
