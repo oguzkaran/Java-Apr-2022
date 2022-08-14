02b52829c8d3c8b601d177f0b23e0865bc8f1f1f
Oğuz Karan <oguzkaran@csystem.org>
2022-08-14 18:30:44 +0300

    Update

--- src/eclipseworkspce/Sample/src/csd/App.java
+++ src/eclipseworkspce/Sample/src/csd/App.java
@@ -1,80 +1,20 @@
 /*-----------------------------------------------------------------------------------------------------------------------	
-	Sınıf Çalışması: Katsayıları klavyeden girilen ikinci dereceden denklemin köklerini bulup ekrana yazdıran 
-	programı yazınız.
-	Açıklamalar:
-	- İki kök varsa örneğin 
-		x1 = 3, x2 = 4 
-	biçiminde ekrana yazılacaktır
-	- Çakışık kök varsa 
-		x1 = x2 = -4 
-	biçiminde ekrana yazılacaktır
-	- Gerçek kök yoksa 
-		Gerçek kök yok!... 
-	biçiminde ekrana yazılacaktır	
-	
-	
-	Sınıfın aşağıdaki gibi kullanım şekli sınıfların yararı 100 birim düşünülürse %1'den daha azdır. Şüphesiz örnek 
-	farklı yaklaşımlarla da yazılabilir. Sınıfın bir kullanımını göstermek için yazılmıştır. İleride daha iyisi yazılacaktır 
+	Java 17 öncesinde [a, b] aralığında rasgele int türden sayı üretimi
 -----------------------------------------------------------------------------------------------------------------------*/
 package csd;
 
 class App {
 	public static void main(String [] args)
 	{
-		SecondOrderEquationSolverApp.run();		
+		java.util.Random random = new java.util.Random();
+		int a = 10;
+		int b = 21;
+		
+		for (int i = 0; i < 20; ++i)
+			System.out.printf("%d ", random.nextInt(b - a + 1) + a); //[a, b] 
+		
+		System.out.println();
 	}
 }
 
-class SecondOrderEquationSolverApp {
-	public static void run()
-	{
-		java.util.Scanner kb = new java.util.Scanner(System.in);
-		
-		System.out.print("İkinci dereceden denklemin katsayılarını giriniz:(a, b, c)");
-		double a = kb.nextDouble();		
-		double b = kb.nextDouble();
-		double c = kb.nextDouble();
-		
-		SecondOrderEquationRootInfo ri = SecondOrderEquationSolver.findRoots(a, b, c);
-		
-		if (ri.exists)
-			System.out.printf("x1 = %f, x2 = %f%n", ri.x1, ri.x2);
-		else
-			System.out.println("Gerçek kök yok");
-	}
-}
 
-class SecondOrderEquationRootInfo {
-	public double x1, x2;
-	public boolean exists;
-}
-
-class SecondOrderEquationSolver {
-	public static void doWorkForSuccess(SecondOrderEquationRootInfo ri, double delta, double a, double b)
-	{
-		double sqrtDelta = Math.sqrt(delta);
-		
-		ri.exists = true;
-		ri.x1 = (-b + sqrtDelta) / (2 * a);
-		ri.x2 = (-b - sqrtDelta) / (2 * a);
-	}
-	
-	
-	public static double getDelta(double a, double b, double c)
-	{
-		return b * b - 4 * a * c;
-	}
-	
-	public static SecondOrderEquationRootInfo findRoots(double a, double b, double c)
-	{
-		double delta = getDelta(a, b, c);
-		
-		SecondOrderEquationRootInfo ri = new SecondOrderEquationRootInfo();
-		
-		if (delta >= 0)
-			doWorkForSuccess(ri, delta, a, b);
-		
-		return ri;		
-	}
-	
-}
