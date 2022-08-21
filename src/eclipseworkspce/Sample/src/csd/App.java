0d1b0247d614afe6f9a67b4ad8ee99ff9b4a195a
Oğuz Karan <oguzkaran@csystem.org>
2022-08-21 18:31:14 +0300

    Update

--- src/eclipseworkspce/Sample/src/csd/App.java
+++ src/eclipseworkspce/Sample/src/csd/App.java
@@ -1,15 +1,11 @@
 /*-----------------------------------------------------------------------------------------------------------------------	
-	Immutable class: Bir nesnenin non-static veri elemanları, sınıf kullanan kodlar (client codes) tarafından 
-	değiştirilemiyorsa bu tarz sınıflara "immutable class" denir. Immutable sınıfların nasıl yazılabileceği ileride
-	detaylı olarak ele alınacaktır. Immutable sınıfların avvantajları ve dezavantajları vardır. Bunlar da zamanla
-	anlaşılacaktır 
+	
 -----------------------------------------------------------------------------------------------------------------------*/
 package csd;
 
 class App {
 	public static void  main(String [] args)
 	{
-		
+				
 	}
 }
-
