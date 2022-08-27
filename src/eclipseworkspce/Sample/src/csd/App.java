99502cb3755d3eca6205d666c5b12bc6fec1051c
Oğuz Karan <oguzkaran@csystem.org>
2022-08-27 18:25:13 +0300

    Update

--- src/eclipseworkspce/Sample/src/csd/App.java
+++ src/eclipseworkspce/Sample/src/csd/App.java
@@ -1,38 +1,19 @@
 /*-----------------------------------------------------------------------------------------------------------------------	
-	Sınıf Çalışması: Parametresi ile aldığı bir yazının tersini döndüren reverse isimli metodu StringUtil sınıfı 
-	içerisinde yazınız ve aşağıdaki kod yazınız  
+	Sınıf Çalışması: Klavyeden elma girilene kadar alınan yazıların arasında - karakteri olacak şekilde birleştirilmiş
+	bir String oluşturan programı yazınız.
+	Örneğin ankara istanbul izmir girilirse -> ankara-istanbul-izmir
+	Örneğin ankara girilirse -> ankara
+	Açıklamalar:
+	- Tamamı whitespace karakterlerinden oluşan String ve boş string eklenmeyecektir 
+	- Programda dizi kullanılmayacaktır
 -----------------------------------------------------------------------------------------------------------------------*/
 package csd;
 
 class App {
 	public static void  main(String [] args)
 	{
-		ReverseTest.run();
+		System.out.println("Java" + 10 + 20);
+		System.out.println("Java" + (10 + 20));
 	}
 }
 
-class ReverseTest {
-	public static void run()
-	{
-		java.util.Scanner kb = new java.util.Scanner(System.in);
-		
-		for (;;) {
-			System.out.print("Bir yazı giriniz:");
-			String str = kb.nextLine();
-			
-			if ("elma".equals(str))
-				break;
-			
-			System.out.printf("Yazının tersi:%s%n", StringUtil.reverse(str));
-		}
-		
-		System.out.println("Tekrar yapıyor musunuz?");
-	}
-}
-
-class StringUtil {
-	public static String reverse(String str)
-	{
-		//TODO:
-	}
-}
