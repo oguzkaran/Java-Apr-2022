1deee84f81cc993ce566be8eb3809c4ba41e6fd9
Oğuz Karan <oguzkaran@csystem.org>
2022-08-28 19:29:09 +0300

    Update

--- src/eclipseworkspce/Sample/src/csd/App.java
+++ src/eclipseworkspce/Sample/src/csd/App.java
@@ -1,88 +1,77 @@
 /*-----------------------------------------------------------------------------------------------------------------------	
-	Homework-005-3. sorunun bir çözümü
-	(Not: Çözüm çalışma sorusunun verildiği tarihte işlenmiş olan konulara göre yazılmıştır)	
+	Sınıf Çalışması: Aşağıda açıklanan metotları StringUtil içerisinde yazınız ve aşağıdaki kod ile test ediniz
+		public static String padLeading(String s, int len, char ch);
+		public static String padLeading(String s, int len);
+		public static String padTrailing(String s, int len, char ch);
+		public static String padTrailing(String s, int len);
+	Açıklamalar:
+	- padLeading metodunun 3 parametreli overload'u ikinci parametresi ile aldığı len, birinci parametresi ile aldığı
+	yazının uzunluğundan büyükse yazıyı soldan (baştan) üçüncü parametresi ile aldığı karakter ile doldurulmnuş yeni
+	bir String referansına geri dönecektir. Örneğin:
+		padLeading("istanbul", 12, 'x'); çağrısı
+		xxxxistanbul 
+	yazısını döndürecektir. 
+	
+	- len yazının uzunluğundan küçük veya eşitse aynı referansa dönecektir
+	
+	- padLeading metodunun 2 parametreli overload'u yazıyı soldan (baştan) space karakteri ile dolduracaktır
+	
+	- padTrailing metotları benzer şekilde sağdanb (sondan) dolduracak şekilde yazılacaktır
+	
+	- Örneği Java 11+ için yazınız
 -----------------------------------------------------------------------------------------------------------------------*/
 package csd;
 
 class App {
-	public static void  main(String [] args)
-	{
-		AreFriendsTest.run();
+	public static void main(String [] args)
+	{	
+		PadLeadingTrailingTest.run();
 	}
 }
 
-
-class AreFriendsTest {
+class PadLeadingTrailingTest {
 	public static void run()
-	{
-		AreFriendsInputTest.run();
-		AreFriends3DigitsTest.run();
-		AreFriends4DigitsTest.run();		
-	}
-}
-
-class AreFriends4DigitsTest {
-	public static void run()
-	{
-		System.out.println("4 basamaklı arkadaş sayılar:");
-		
-		for (int a = 1000; a <= 9999; ++a)
-			for (int b = a + 1; b <= 9999; ++b)
-				if (NumberUtil.areFriends(a, b))
-					System.out.printf("(%d, %d)%n", a, b);
-	}
-}
-
-class AreFriends3DigitsTest {
-	public static void run()
-	{
-		System.out.println("3 basamaklı arkadaş sayılar:");
-		
-		for (int a = 100; a <= 999; ++a)
-			for (int b = a + 1; b <= 999; ++b)
-				if (NumberUtil.areFriends(a, b))
-					System.out.printf("(%d, %d)%n", a, b);
-	}
-}
-
-class AreFriendsInputTest {
-	public static void run()
-	{
+	{		
 		java.util.Scanner kb = new java.util.Scanner(System.in);
 		
 		for (;;) {
-			System.out.print("İki sayı giriniz:");
-			int a = kb.nextInt();
-			int b = kb.nextInt();
+			System.out.print("Bir yazı giriniz:");
+			String s = kb.nextLine();
 			
-			if (a == 0)
+			if ("elma".equals(s))
 				break;
 			
-			if (NumberUtil.areFriends(a, b))
-				System.out.printf("%d ve %d sayıları arkadaştır%n", a, b);
-			else
-				System.out.printf("%d ve %d sayıları arkadaş değildir%n", a, b);
+			System.out.println("Bir sayı giriniz:");
+			int len = Integer.parseInt(kb.nextLine());
+			
+			System.out.printf("(%s)%n", StringUtil.padLeading(s, len, 'x'));
+			System.out.printf("(%s)%n", StringUtil.padLeading(s, len));
+			System.out.printf("(%s)%n", StringUtil.padTrailing(s, len, 'x'));
+			System.out.printf("(%s)%n", StringUtil.padTrailing(s, len));
 		}
 		
 		System.out.println("Tekrar yapıyor musunuz?");
-	}
+	}	
 }
 
-class NumberUtil {
-	public static boolean areFriends(int a, int b)
+class StringUtil {
+	public static String padLeading(String s, int len, char ch)
 	{
-		return sumFactors(a) == b && sumFactors(b) == a;
+		//TODO:
 	}
 	
-	public static int sumFactors(int a)
+	public static String padLeading(String s, int len)
 	{
-		int result = 1;
-		int sqrtVal = (int)Math.sqrt(a);
-		
-		for (int i = 2; i <= sqrtVal; ++i)
-			if (a % i == 0)
-				result += (i == a / i) ? i : (i + a / i);
-		
-		return result;		
+		//TODO:
+	}
+	
+	public static String padTrailing(String s, int len, char ch)
+	{
+		//TODO:
+	}
+	
+	public static String padTrailing(String s, int len)
+	{
+		//TODO:
 	}
 }
