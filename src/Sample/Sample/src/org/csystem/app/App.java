18baa3d244da9e6d2ec3aa46d5bbe834b9addaa0
Oğuz Karan <oguzkaran@csystem.org>
2022-10-02 20:22:33 +0300

    Update

--- src/Sample/Sample/src/org/csystem/app/App.java
+++ src/Sample/Sample/src/org/csystem/app/App.java
@@ -1,37 +1,82 @@
 /*-----------------------------------------------------------------------------------------------------------------------
-	Sınıfın protected bölümü farklı paketlerdeki diğer sınıflar için "türetme/kalıtım (inheritance)" söz konusu değilse
-	private anlamındadır. Farklı pakette, ancak "türemiş sınıf (derived/sub class)" kendisine aşt protected bölüme
-	erişebilir. Türetme ve protected bölümün anlamı ileride detaylı olarak ele alınacaktır
+	Aşağıdaki durumlardan en az biri varsa veri elemanının gizlenmesi gerekir:
+	- Sınıfın geliştirilmesi aşamalarında versiyon ilerledikçe bir veri elemanının isminin hatta türünün değişmesi durumu
+	ile çok fazla karşılaşılır. Bu durumda daha önce yazılmış olan kofların bu değişimden etkilenmemesi için veri elemanı
+	gizlenmelidir.
+
+	- Bir veri elemanının değeri değiştiğinde başka bir veri elemanının değerinin değişen veri elemanına göre yeniden
+	belirlenmesi gerekebilir. Bu durumda da veri elemanı gizlenmelidir.
+
+	- Bir veri elemanının değeri değiştiğinde yeni değere göre bazı işlemlerin yapılması gerekebilir. Örneğin bir veri
+	elemanı açık olan işlem yapılan bir dosyanın ismini tutuyor olabilir. Bu durumda bu veri değiştiğinde eski dosyanın
+	kapatılıp yeni dosyanın açılması gerektiği gibi bir durumda veri elemanı gizlenmelidir
+
+	- Bir veri elemanının sınır değerleri olabilir. Bu değerlerin kontrol edilip değiştirilebilmesi için veri elemanı
+	gizlenmelidir
+
+	Bazen bu durumlar dışındaki özel durumlarda da gizleme yapılabilir. Bu tarz durumlar ileride ele alınacaktır.
+
+	Yukarıdaki durumlar dışında (%3 ile %5 arası) sınıfın veri elemanları public yapılabilir
 -----------------------------------------------------------------------------------------------------------------------*/
 package org.csystem.app;
 
 class App {
 	public static void main(String [] args)
 	{
+		Date date = new Date(6, 9, 2021);
+
+		System.out.printf("%02d/%02d/%04d%n", date.getDay(), date.getMonth(), date.getYear());
+
+		date.setDay(10);
+		date.setYear(1976);
+
+		System.out.printf("%02d/%02d/%04d%n", date.getDay(), date.getMonth(), date.getYear());
 	}
 }
 
-class B {
-	public void bar()
-	{
-		A a = new A();
+class Date {
+	private int m_day, m_month, m_year;
 
-		a.x = 20;
-		a.foo();
-	}
-}
-
-
-
-class A {
-	protected int x;
-	protected A()
+	public Date(int day, int month, int year)
 	{
 		//...
+		m_day = day;
+		m_month = month;
+		m_year = year;
 	}
 
-	protected void foo()
+	public int getDay()
+	{
+		return m_day;
+	}
+
+	public void setDay(int day)
 	{
 		//...
+		m_day = day;
 	}
+
+	public int getMonth()
+	{
+		return m_month;
+	}
+
+	public void setMonth(int month)
+	{
+		//...
+		m_month = month;
+	}
+
+	public int getYear()
+	{
+		return m_year;
+	}
+
+	public void setYear(int year)
+	{
+		//...
+		m_year = year;
+	}
+
+	//...
 }
