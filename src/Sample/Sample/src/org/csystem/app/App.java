e7cef53137ec9550084a637501c04e71afbd32cb
Oğuz Karan <oguzkaran@csystem.org>
2023-01-19 19:17:07 +0300

    Update

--- src/Sample/Sample/src/org/csystem/app/App.java
+++ src/Sample/Sample/src/org/csystem/app/App.java
@@ -1,12 +1,37 @@
 /*-----------------------------------------------------------------------------------------------------------------------
-    Aşağıdaki örneği çeşitli değerler ile çalıştırıp sonucları gözlemleyiniz
+    Aşağıdaki örnekte run metodu NaNException fırlatıldığında onu yakalamış ve aynı nesneyi yeniden fırlatmıştır. Bu işleme
+    "rethrow" denir. Örnekte run metodu müşteri kodlar açısından NaNExcption da fırlatabilir durumdadır. Bu run metodunun
+    dökumanlarına yazılır. run metodunu yazan programcı açısından ise NanException ham ilgili try bloğunda işlenmiş (handling).
+    hem de aynı exceptiıon nesnesi müşteri koda fırlatılmış olur
 -----------------------------------------------------------------------------------------------------------------------*/
 package org.csystem.app;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 class App {
     public static void main(String[] args)
+    {
+        try {
+            Application.run();
+            //...
+        }
+        catch (InfinityException ignore) {
+            System.out.println("Undefined");
+        }
+        catch (NaNException ignore) {
+            System.out.println("main:Indeterminate");
+        }
+        catch (InputMismatchException ignore) {
+            System.out.println("Input mismatch");
+        }
+
+        System.out.println("Tekrar yapıyor musunuz?");
+    }
+}
+
+class Application {
+    public static void run()
     {
         try {
             Scanner kb = new Scanner(System.in);
@@ -16,14 +41,12 @@ class App {
 
             System.out.printf("log(%.2f) = %f%n", val, MathUtil.log(val));
         }
-        catch (InfinityException ignore) {
-            System.out.println("Undefined");
-        }
-        catch (NaNException ignore) {
+        catch (NaNException ex) {
             System.out.println("Indeterminate");
+            throw ex; //rethrow
         }
 
-        System.out.println("Tekrar yapıyor musunuz?");
+        System.out.println("run sonu!...");
     }
 }
 
@@ -37,14 +60,20 @@ class MathUtil {
             throw new NaNException();
 
         return Math.log(a);
-
     }
 }
 
-class InfinityException extends RuntimeException {
+
+class InfinityException extends MathException {
 
 }
 
-class NaNException extends RuntimeException {
+class NaNException extends MathException {
 
 }
+
+
+class MathException extends RuntimeException {
+
+}
+
