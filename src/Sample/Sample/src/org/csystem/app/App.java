67f1f57973f5f56c04b5fb5067862b5f5df11e91
Oğuz Karan <oguzkaran@csystem.org>
2023-02-22 19:27:47 +0300

    Update

--- src/Sample/Sample/src/org/csystem/app/App.java
+++ src/Sample/Sample/src/org/csystem/app/App.java
@@ -1,116 +1,65 @@
 /*-----------------------------------------------------------------------------------------------------------------------
-    Java'da exception sınıfları kategori olarak iki gruba ayrılır: checked, unchecked
+    throws bildirimi olan bir metodun override edilmesi durumunda override edilen metotta throws listesinde olan exception
+    sınıfları kaldırılabilir. Yani örneğin override edilmiş olan hiç throws bildirimi olmayabilir. override edilen
+    metotta thrpws listesindeki sınıflar veya onlardan doğrudan ya da dolaylı olarak türetilmiş olan exceprtion sınıfları
+    bulunabilir. Aksi durumlarda error oluşur
 -----------------------------------------------------------------------------------------------------------------------*/
 package org.csystem.app;
-
-import org.csystem.util.console.Console;
 
 class App {
     public static void main(String[] args)
     {
-        try {
-            Application.run();
-        }
-        catch (WrapperException ex) {
-            Console.writeLine("Message:%s", ex.getMessage());
-            Throwable cause = ex.getCause();
-            if (cause != null)
-                Console.writeLine("Cause Excption Message:%s", cause.getMessage());
-        }
 
-        Console.writeLine("Tekrar yapıyor musunuz?");
     }
 }
 
-class Application {
-    public static void run()
+class E extends A {
+    public void foo() throws TheirException //error
     {
-        try {
-            double val = Console.readDouble("Bir sayı giriniz:", "Hatalı giriş yaptınız!...");
-
-            Console.writeLine("log(%.2f) = %f", val, MathUtil.log(val));
-        }
-        catch (NegativeInfinityException ex) {
-            //...
-            throw new WrapperException("Application.run: NegativeInfinityException", ex);
-        }
-        catch (NaNException ignore) {
-            throw new WrapperException("Application.run: NaNException");
-        }
+        //...
     }
 }
 
-class WrapperException extends RuntimeException {
-    public WrapperException(String message)
+class D extends A {
+    public void foo() throws OurException
     {
-        this(message, null);
+        //...
     }
+}
 
-    public WrapperException(String message, Throwable cause)
+class C extends A {
+    public void foo() throws MyException
     {
-        super(message, cause);
-    }
-
-    public String getMessage()
-    {
-        Throwable cause = getCause();
-
-        return String.format("Message:%s%s", super.getMessage(), cause != null ? ", Cause Message:" + cause.getMessage() : "");
+        //...
     }
 }
 
 
 
-class MathUtil {
-    public static double log(double a)
+class B extends A {
+    public void foo()
     {
-        if (a == 0)
-            throw new NegativeInfinityException("Undefined");
-
-        if (a < 0)
-            throw new NaNException("Indeterminate");
-
-        return Math.log(a);
+        //...
     }
 }
 
-class NegativeInfinityException extends MathException {
-    public NegativeInfinityException(String message)
-    {
-        super(message, MathExceptionStatus.NEGATIVE_INFINITY);
-    }
-
-    public String getMessage()
-    {
-        return String.format("Message:%s, Status:%s", super.getMessage(), getMathExceptionStatus());
-    }
+abstract class A {
+    public abstract void foo() throws MyException, YourException;
+    //...
 }
 
-class NaNException extends MathException {
-    public NaNException(String message)
-    {
-        super(message, MathExceptionStatus.NAN);
-    }
-
-    public String getMessage()
-    {
-        return String.format("Message:%s, Status:%s", super.getMessage(), getMathExceptionStatus());
-    }
+class MyException extends Exception {
+    //...
 }
 
-enum MathExceptionStatus {ZERO, POSITIVE_INFINITY, NEGATIVE_INFINITY, POSITIVE_ZERO, NEGATIVE_ZERO, NAN,}
-
-class MathException extends RuntimeException {
-    private final MathExceptionStatus m_mathExceptionStatus;
-    public MathException(String message, MathExceptionStatus mathExceptionStatus)
-    {
-        super(message);
-        m_mathExceptionStatus = mathExceptionStatus;
-    }
-
-    public MathExceptionStatus getMathExceptionStatus()
-    {
-        return m_mathExceptionStatus;
-    }
+class YourException extends Exception {
+    //...
 }
 
+class OurException extends MyException {
+    //...
+}
+
+class TheirException extends Exception {
+    //...
+}
