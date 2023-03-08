b13933523a42ceb94fa73038fad1e50cadcf4efe
Oğuz Karan <oguzkaran@csystem.org>
2023-03-08 21:14:55 +0300

    Update

--- src/Sample/Sample/src/org/csystem/app/App.java
+++ src/Sample/Sample/src/org/csystem/app/App.java
@@ -1,30 +1,24 @@
 /*-----------------------------------------------------------------------------------------------------------------------
-    Java 9 ile birlikte arayüzler içerisinde private metotlar yazılabilir. Şüphesiz bu metotların gövdeleri olmalıdır.
-    Yani abstract olamazlar. private metotlar için default anahtar sözcüğü kullanılamaz
+    Bir sınıf birden fazla arayüzü destekleyebilir
 -----------------------------------------------------------------------------------------------------------------------*/
 package org.csystem.app;
-
-import org.csystem.util.console.Console;
 
 class App {
     public static void main(String[] args)
     {
-        Util.print(Integer::sum, 10, 20);
-        Util.print((a, b) -> a * b, 10, 20);
-        Util.print((a, b) -> 2 * a + b, 10, 20);
+
     }
 }
 
-class Util {
-    public static void print(IIntBinaryOperator intBinaryOperator, int a, int b)
-    {
-        Console.writeLine(intBinaryOperator.applyAsInt(a, b));
-    }
-}
-
-interface IIntBinaryOperator {
-    int applyAsInt(int a, int b);
+class A implements IX, IY {
+    //...
 }
 
 
+interface IX {
+    //...
+}
 
+interface IY {
+    //...
+}
