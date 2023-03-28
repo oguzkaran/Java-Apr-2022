3480ed031570b92310033aa8ab3d95b03b963292
Oğuz Karan <oguzkaran@csystem.org>
2023-03-28 19:02:50 +0300

    Update

--- src/Sample/Sample/src/org/csystem/app/App.java
+++ src/Sample/Sample/src/org/csystem/app/App.java
@@ -1,35 +1,62 @@
 /*-----------------------------------------------------------------------------------------------------------------------
-     Bazı durumlarda nesne içerisinde bir takım kaynaklar (resources) kullanılıyor olabilir. Bu kaynakların kullanılmaya
-     başlanmasına mantıksal olarak "kaynağın açılması (open)" dersek kaynak kullanımı bittiğinde, mantıksal olarak
-     "kapatılması (close)" yani artık serbest bırakılması gerekebilir. Java'da nesneler garbage collector tarafından yok
-     edildiğinden kullanılan kaynağın geri bırakılması da nesnenin yok edilmesi durumundan  gerçekleşir. Anımsanacağı gibi
-     garbage collector'ün ne zaman devreye gireceği belirsizdir. Çünkü örneğin bu kaynağı iş bittikten sonra başka bir nesne
-     kullanmak isteyebilir.  Burada kaynak olarak belirtilen bir dosya, bir cihaz, bir resim vb. olabilir. Böylesi bir
-     durumda convention olarak Closeable arayüzü kullanılır. Closeable arayüzünün close isimli abstract metodu vardır.
+
 -----------------------------------------------------------------------------------------------------------------------*/
 package org.csystem.app;
+
+import org.csystem.util.console.Console;
+
+import java.io.Closeable;
+import java.io.IOException;
+import java.util.Random;
 
 class App {
     public static void main(String[] args)
     {
-        A a = new B();
-        IX ix;
-
-        ix = (IX)a; //haklı dönüşüm
-
-        System.out.println("Tekrar yapıyor musunuz?");
+        try (Connection con = new Connection("postgresql:jdbc://192.168.1.123:5432/weatherappdb")) {
+            con.doWork();
+        }
+        catch (IllegalStateException ex) {
+            Console.writeLine("Illegal State:%s", ex.getMessage());
+        }
+        catch (IllegalArgumentException ex) {
+            Console.writeLine("Illegal Argument:%s", ex.getMessage());
+        }
+        catch (Throwable ignore) {
+            //...
+        }
     }
 }
 
 
-class B extends A {
-    //...
-}
+class Connection implements Closeable {
+    private final String m_url;
 
-class A  {
-    //...
-}
+    public Connection(String url)
+    {
+        //...
+        Random r = new Random();
 
-interface IX {
-    //...
+        if (r.nextBoolean())
+            throw new IllegalArgumentException("Url not found");
+
+        m_url = url;
+        Console.writeLine("Connected to %s", url);
+    }
+
+    public void doWork()
+    {
+        Random r = new Random();
+
+        //...
+
+        if (r.nextBoolean())
+            throw new IllegalStateException(("Illegal operation"));
+
+        Console.writeLine("Working on connection at '%s'", m_url);
+    }
+
+    public void close() throws IOException
+    {
+        Console.writeLine("Connection closed");
+    }
 }
